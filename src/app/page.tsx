55595e1bfcebd17df57bf0ee29e56c7cd9262884
Mateus Lopes <mateusmlopestr@gmail.com>
2025-02-01 11:01:50 -0300

    fix: :bug: fix main page import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Topbar from "@/components/topbar";
 import Footer from "@/components/footer";
 import Hero from "@/components/sections/hero";
 import Projects from "@/components/sections/projects";
-import Experience from "@/components/sections/experience";
 
 export default function Home() {
   return (
